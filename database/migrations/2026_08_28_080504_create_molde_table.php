de65e9e82b2a07c90e2f0781010b988bb7396784
Cristhian Martinez <alex95svr@gmail.com>
2026-08-28 08:18:32 -0500

    Se creo las tablas molde, y se realizo el foreign key con la tabla inspecciones_cavidades

---
+++ database/migrations/2026_08_28_080504_create_molde_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('molde', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo')->unique();
+            $table->string('nombre');
+            $table->integer('numero_cavidades');
+            $table->boolean('activo')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('molde');
+    }
+};
